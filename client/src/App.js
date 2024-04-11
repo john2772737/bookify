62a803f0163943bb82d982ef7477a183cee15727
JADA <jaduhlgn@gmail.com>
2024-04-11 21:55:53 +0800

    jada

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,15 @@ function App() {
   return (
     <div className="App">
      <h1>HELLO JADA</h1>
-    </div>
+     <p>this is a paragraph.</p>
+      <section className='Section1'>
+        <h2>Cpe 2A</h2>
+        <nav class="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
+          
+        </nav>
+
+      </section>
+    </div> 
   );
 }
 
